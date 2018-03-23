01bcd614595e49605f432969190b499a76a16d66
Jacob Truman <jacob.truman@gmail.com>
2018-03-23 16:17:11 -0600

    Add missing package to install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Jacob Truman',
     author_email='jacob.truman@gmail.com',
     url='https://github.com/jacobtruman/TVShowFetch',
-    packages=['tv_show_fetch'],
+    packages=['tv_show_fetch', 'fetchers'],
     scripts=['bin/TVShowFetchRunner.py'],
     install_requires=['requests>=2.11.0', 'beautifulsoup4', 'youtube_dl', 'pycrypto'],
     dependency_links=[]
